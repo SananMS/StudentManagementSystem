0e6c07f315d8271505990750b23bd78f5b5f919c
Sanan <mammadovsanan98@gmail.com>
2023-03-02 19:37:50 +0200

    Repository finished

--- src/main/java/com/example/repositories/StudentRepository.java
+++ src/main/java/com/example/repositories/StudentRepository.java
@@ -1,4 +1,8 @@
 package com.example.repositories;
 
-public class StudentRepository {
-}
+import com.example.models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {}
